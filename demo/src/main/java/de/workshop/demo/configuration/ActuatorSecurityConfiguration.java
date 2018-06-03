561a15eb65c8658f4fca4a35f5146b3854125e59
onkeliroh <danielpollack@posteo.de>
2018-06-03 20:38:01 +0200

    add user roles

--- demo/src/main/java/de/workshop/demo/configuration/ActuatorSecurityConfiguration.java
+++ demo/src/main/java/de/workshop/demo/configuration/ActuatorSecurityConfiguration.java
@@ -1,9 +1,12 @@
 package de.workshop.demo.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +14,23 @@ public class ActuatorSecurityConfiguration extends WebSecurityConfigurerAdapter 
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/actuator/**").hasRole("ADMIN").and()//
+				.formLogin().permitAll().and()//
+				.logout().permitAll();//
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("123456").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+	}
+
+	/**
+	 * since spring 5 one must add an password encoder
+	 */
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 }
