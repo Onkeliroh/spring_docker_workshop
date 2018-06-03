c9ebc5cace128210f3a734eb1f0197bc7db16048
onkeliroh <danielpollack@posteo.de>
2018-06-03 13:19:34 +0200

    add custom metric counter

--- demo/src/main/java/de/workshop/demo/controller/HelloController.java
+++ demo/src/main/java/de/workshop/demo/controller/HelloController.java
@@ -1,13 +1,23 @@
 package de.workshop.demo.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private final Counter counter;
+
+	public HelloController(MeterRegistry meterRegistry) {
+		counter = meterRegistry.counter("de.workshop.demo.controller.hellocontroller.calls");
+	}
+
+
 	@GetMapping("/hello")
 	public String sayHello() {
+		counter.increment();
 		return "Hello World";
 	}
 }
